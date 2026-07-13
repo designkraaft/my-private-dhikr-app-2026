8dd01eddcdcb94a712cb329803cf1d8daf888c4a
designkraft <khaldonhamdo2022@gmail.com>
2026-07-13 09:00:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dhikr-app-pro-v2';
+const CACHE_NAME = 'dhikr-app-pro-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -6,9 +6,26 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  // تفعيل النسخة الجديدة فوراً
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  // حذف الكاش القديم
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
